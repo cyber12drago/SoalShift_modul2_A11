033c4ef07ec08dda0b540f047cab38f072fafbe4
cyber12drago <naufal.refadi@yahoo.co.id>
2019-03-15 15:08:37 +0700

    soal2

---
+++ soal2.c
@@ -0,0 +1,65 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
+
+int main() {
+  pid_t pid, sid;
+
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/home/cyber12drago/Modul_2")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while(1) {
+    char nama_file[]="/home/cyber12drago/Modul_2/hatiku/elen.ku";
+    struct stat sta;
+    char cek[]="www-data";
+
+    if(stat(nama_file,&sta)==0){
+	char mode[] = "0777";
+        int i;
+        i = atoi(mode);
+        chmod(nama_file,i);
+
+        struct passwd *pw = getpwuid(sta.st_uid);
+        struct group *gr = getgrgid(sta.st_gid);
+
+        if(((strcmp(pw->pw_name,cek))==0) && ((strcmp(gr->gr_name,cek))==0))
+	{
+		remove(nama_file);
+        }
+    }
+    sleep(3);
+  }
+  
+  exit(EXIT_SUCCESS);
+}
